eac14f416d3d56ac9440a3dab600121c978f79ac
Nico Kratky <nico@nicokratky.me>
2017-08-21 08:11:40 +0200

    I had no idea what I was doing

--- Server.py
+++ Server.py
@@ -1,25 +1,4 @@
 import sys
-import GSDEP
-
-ip = sys.argv[1] if len(sys.argv) > 1 else '192.168.101.52'
-port = sys.argv[2] if len(sys.argv) == 3 else 1337
-
-s = GSDEP.Server(ip, port)
-
-def sensor():
-	sense = SenseHat()
-	raw = sense.get_accelerometer_raw()
-	return [raw['x'], raw['y'], raw['z']]
-
-s.attach_readout_function(sensor)
-
-try:
-	s.start()
-except KeyboardInterrupt:
-	s.shutdown()
-
-
-import sys
 from GSDEP import Server, GSDEPHandler, CHANNELS, CMDS
 from time import sleep
 import random
